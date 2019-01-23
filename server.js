fb7a946164899d0d88764cda937cf78f548d854d
Nicholas MacArthur <n.j.macarthur@gmail.com>
2019-01-23 15:07:19 +1000

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const validSlug = async slug => {
     `https://cdn.contentful.com/spaces/g0l9uemxdyxt/environments/master/entries?access_token=7646a8b0e6279a63315a1a9bc526427183a8924429036970202be98294e1caf3&content_type=guide&fields.slug=${slug}`
   );
   const json = await res.json();
-  return json.items.length < 0;
+  return json.items.length > 0;
 };
 
 app
