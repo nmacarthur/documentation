76a61c82b19ab389489fefe9f53ec1ba1db091cf
Nicholas MacArthur <n.j.macarthur@gmail.com>
2019-01-31 17:31:09 +1000

    Updates

--- pages/post.js
+++ pages/post.js
@@ -1,13 +1,9 @@
 import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
 
 import { BLOCKS } from '@contentful/rich-text-types';
-import { getSinglePost, getAllPosts } from '../util/dataFetching';
+import { getSinglePost, getAllPosts, getAssets } from '../util/dataFetching';
 
 import Layout from '../components/MyLayout';
-
-import { RichTextParser } from '../util/richtextParser';
-
-import { getImage } from '../util/dataFetching';
 
 const HtmlToReactParser = require('html-to-react').Parser;
 
@@ -15,30 +11,27 @@ const htmlToReactParser = new HtmlToReactParser();
 
 const options = {
   renderNode: {
-    [BLOCKS.EMBEDDED_ASSET]: node => {
-      const data = getImage({ id: node.data.target.sys.id });
-      return `<img src='' alt='oi' />`;
-    }
+    [BLOCKS.EMBEDDED_ASSET]: node => `<img src='' alt='oi' />`
   }
 };
 
-const Content = ({ content, title }) => {
+const Content = ({ content, title, slug }) => {
   const data = documentToHtmlString(content, options);
   const parsed = htmlToReactParser.parse(data);
+
   return <p>{parsed}</p>;
 };
 
-const Page = ({ sidebar, data }) => (
+const Page = async ({ sidebar, data, slug }) => (
   <Layout sidebar={sidebar}>
-    <Content title={data.title} content={data.content} />
+    <Content title={data.title} content={data.content} slug={slug} />
   </Layout>
 );
 
 Page.getInitialProps = async props => {
   const sidebar = await getAllPosts();
   const data = await getSinglePost(props);
-
-  return { sidebar: sidebar.data, data: data.data, assets: data.assets };
+  return { sidebar: sidebar.data, data: data.data, slug: props.query.slug };
 };
 
 export default Page;
--- util/dataFetching.js
+++ util/dataFetching.js
@@ -1,14 +1,5 @@
 import fetch from 'isomorphic-fetch';
 
-const contentful = require('contentful');
-
-const client = contentful.createClient({
-  // This is the space ID. A space is like a project folder in Contentful terms
-  space: 'g0l9uemxdyxt',
-  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-  accessToken: '07646a8b0e6279a63315a1a9bc526427183a8924429036970202be98294e1caf3'
-});
-
 const getSinglePost = async props => {
   const res = await fetch(
     `https://cdn.contentful.com/spaces/g0l9uemxdyxt/environments/master/entries?access_token=7646a8b0e6279a63315a1a9bc526427183a8924429036970202be98294e1caf3&content_type=guide&fields.slug=${
@@ -28,12 +19,12 @@ const getAllPosts = async () => {
   return { data: json.items };
 };
 
-const getImage = async ({ id }) => {
+const getAssets = async slug => {
   const res = await fetch(
-    `https://cdn.contentful.com/spaces/g0l9uemxdyxt/environments/master/assets/${id}?access_token=7646a8b0e6279a63315a1a9bc526427183a8924429036970202be98294e1caf3`
+    `https://cdn.contentful.com/spaces/g0l9uemxdyxt/environments/master/entries?access_token=7646a8b0e6279a63315a1a9bc526427183a8924429036970202be98294e1caf3&content_type=guide&fields.slug=${slug}&include=1`
   );
   const json = await res.json();
-  return { data: json };
+  return json.includes.Asset;
 };
 
-export { getSinglePost, getAllPosts, getImage };
+export { getSinglePost, getAllPosts, getAssets };
