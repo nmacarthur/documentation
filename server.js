fa8292c2eba5e20b0a227e39dcd2e14f1f360940
Nicholas MacArthur <n.j.macarthur@gmail.com>
2019-03-05 17:27:56 +1000

    updates

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
+const port = process.env.PORT || 3000;
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
@@ -33,9 +34,9 @@ app
 
     server.get('*', (req, res) => handle(req, res));
 
-    server.listen(3000, err => {
+    server.listen(port, err => {
       if (err) throw err;
-      console.log('> Ready on http://localhost:3000');
+      console.log('> Ready');
     });
   })
   .catch(ex => {
