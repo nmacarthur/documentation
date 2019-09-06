cfdcaa0bcb2dcb5bd786c229d2f1db5e4da3e52b
Nicholas MacArthur <n.j.macarthur@gmail.com>
2019-09-06 16:59:38 +1000

    updates

--- next.config.js
+++ next.config.js
@@ -11,7 +11,29 @@ const { PHASE_PRODUCTION_SERVER } =
 module.exports = (phase, { defaultConfig }) => {
   if (phase === PHASE_PRODUCTION_SERVER) {
     // Config used to run in production.
-    return {};
+    const withCSS = require('@zeit/next-css');
+    const withSCSS = require('@zeit/next-sass');
+
+    return withCSS(
+      withSCSS({
+        webpack(config, options) {
+          config.module.rules.push({
+            test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+            use: {
+              loader: 'url-loader',
+              options: {
+                limit: 100000
+              }
+            }
+          });
+          return config;
+        },
+        env: {
+          CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
+          CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN
+        }
+      })
+    );
   }
 
   const withCSS = require('@zeit/next-css');
@@ -29,9 +51,11 @@ module.exports = (phase, { defaultConfig }) => {
             }
           }
         });
-        config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
-
         return config;
+      },
+      env: {
+        CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
+        CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN
       }
     })
   );
